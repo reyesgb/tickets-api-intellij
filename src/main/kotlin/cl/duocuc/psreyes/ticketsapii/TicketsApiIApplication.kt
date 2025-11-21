9f36dec6a6cd391c875e4a11945dcbc75ccf0c94
Pablo sebastian reyes revilla <ps.reyes@duocuc.cl>
2025-11-21 20:20:36 -0300

    Parte 2 del proyecto.

---
+++ src/main/kotlin/cl/duocuc/psreyes/ticketsapii/TicketsApiIApplication.kt
@@ -0,0 +1,11 @@
+package cl.duocuc.psreyes.ticketsapii
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketsApiIApplication
+
+fun main(args: Array<String>) {
+    runApplication<TicketsApiIApplication>(*args)
+}
